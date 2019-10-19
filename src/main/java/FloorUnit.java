3ca3c8372d51390b28b477172d6e696572096f32
Rolando Ramirez <rolando.ramirezx@gmail.com>
2019-10-19 14:41:44 -0500

    adding maven project files

---
+++ src/main/java/FloorUnit.java
@@ -0,0 +1,12 @@
+public class FloorUnit{
+    //instance variables
+    int dirtLevel;
+    int obstacle;
+
+    //constructor for FloorUnit
+    public FloorUnit(int dirtLevel, int obstacle){
+        this.dirtLevel = dirtlevel;
+        this.obstacle = obstacle;
+    }
+
+}
