8f63f2773ca5662d4f962bc1762f96185826885b
Rolando Ramirez <rolando.ramirezx@gmail.com>
2019-10-19 16:46:36 -0500

    adding project config updates

---
+++ src/main/java/System/Dummy.java
@@ -0,0 +1,12 @@
+package System;
+
+public class Dummy{
+
+    public void DummyPrint(){
+        System.out.println("dummy print");
+    }
+
+    public static void main(String[] args){
+        System.out.print("hello");
+    }
+}
---
+++ src/main/test/System/DummyTest.java
@@ -0,0 +1,13 @@
+package System;
+
+import System.Dummy;
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void DummyPrintTest(){
+        Dummy dummy = new Dummy();
+        dummy.DummyPrint();
+    }
+}
