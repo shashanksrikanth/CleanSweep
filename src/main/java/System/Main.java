13863e9de42189a4af9b8e46e6421d3cb1d6855d
shashanksrikanth1 <ssrikan1@mail.depaul.edu>
2019-10-26 20:56:45 -0500

    Adding control system basic functionality

---
+++ src/main/java/System/Main.java
@@ -0,0 +1,15 @@
+package System;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[]args) throws IOException {
+        SensorSystem sensor = new SensorSystem();
+        sensor.readFile("/Users/shashanksrikanth/Desktop/floorplan.txt");
+        ControlSystem control = new ControlSystem(sensor);
+        int count = sensor.getCount();
+        for(int i=0; i<15; i++){
+            control.moveDevice();
+        }
+    }
+}
