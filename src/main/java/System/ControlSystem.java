8bcabd9b121a122e48a1b52881d245c83338ac13
shashanksrikanth1 <ssrikan1@mail.depaul.edu>
2019-10-26 20:56:39 -0500

    Adding control system basic functionality

--- src/main/java/System/ControlSystem.java
+++ src/main/java/System/ControlSystem.java
@@ -1,4 +1,58 @@
 package System;
 
 public class ControlSystem {
+
+    //instance variables
+    SensorSystem sensor;
+    int batteryLevel;
+    int[]position;
+
+    public ControlSystem(SensorSystem sensor){
+        this.sensor = sensor;
+        batteryLevel = 100;
+        position = new int[2];
+        position[0]= 0;
+        position[1]=0;
+    }
+
+    public void moveDevice (int x, int y){
+        position[0]=x;
+        position[1]=y;
+    }
+
+    public int[] checkWhereToMove(){
+        //where next to move
+        int[]coordinates = new int[2];
+        //check if anything is down (per our POV)
+        if(!sensor.isObstacle(position[0], position[1]+1) && !sensor.isVisited(position[0], position[1]+1)) {
+            System.out.println("Checking "+position[0]+", "+(position[1]+1));
+            coordinates[0]=0;
+            coordinates[1]=1;
+        }
+        //check if anything is in the right (per our POV)
+        else if(!sensor.isObstacle(position[0]+1, position[1]) && !sensor.isVisited(position[0]+1, position[1])){
+            System.out.println("Checking "+(position[0]+1)+", "+position[1]);
+            coordinates[0]=1;
+            coordinates[1]=0;
+        }
+        //check if anything is on the left (per our POV)
+        else if(!sensor.isObstacle(position[0]-1, position[1]) && !sensor.isVisited(position[0]-1, position[1])){
+            System.out.println("Checking "+(position[0]-1)+", "+position[1]);
+            coordinates[0]=-1;
+            coordinates[1]=0;
+        }
+        else{
+            coordinates[0]=0;
+            coordinates[1]=-1;
+        }
+        return coordinates;
+    }
+
+    public void moveDevice(){
+        int[]whereToGo = this.checkWhereToMove();
+        position[0]+=whereToGo[0];
+        position[1]+=whereToGo[1];
+        sensor.setVisited(position[0],position[1]);
+        System.out.println("Machine is currently at ["+position[0]+","+position[1]+"]");
+    }
 }
--- src/main/java/System/SensorSystem.java
+++ src/main/java/System/SensorSystem.java
@@ -4,34 +4,44 @@ import java.io.*;
 public class SensorSystem {
 
     FloorUnit[][]floorPlan;
+    int dimensions;
+    int count;
     public boolean isDirt(int row, int col){
         return floorPlan[row][col].getIsDirt();
     }
 
     //getter method for determining if there is an obstacle in a unit
     public boolean isObstacle(int row, int col){
-        return floorPlan[row][col].getIsObstacle();
+        if(row<dimensions && col<dimensions) return floorPlan[row][col].getIsObstacle();
+        else return true;
     }
-
+    public boolean isVisited(int row, int col){
+        if(row<dimensions && col<dimensions) return floorPlan[row][col].getIsVisited();
+        else return true;
+    }
     //once the control system cleans a unit, we want to update the floor plan saying that the unit is no longer dirty.
     public void setDirt (int row, int col, int dirtLevel){
         floorPlan[row][col].setDirtLevel(dirtLevel);
     }
+    public void setVisited(int row, int col) {floorPlan[row][col].visited=1;count--;}
+    public int getCount(){return count;}
 
     //reading in a floor system file
     public void readFile (String fileName) throws IOException{
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            int dimensions = Integer.parseInt(reader.readLine());
+            dimensions = Integer.parseInt(reader.readLine());
+            count=dimensions;
             floorPlan = new FloorUnit[dimensions][dimensions];
             String line = reader.readLine();
-            while(line!=null) {
-                String[]information = line.split(",");
+            while (line != null) {
+                String[] information = line.split(",");
                 int rowNum = Integer.parseInt(information[0]);
                 int colNum = Integer.parseInt(information[1]);
                 int dirtLevel = Integer.parseInt(information[2]);
                 int obstacle = Integer.parseInt(information[3]);
-                floorPlan[rowNum][colNum] = new FloorUnit(dirtLevel, obstacle);
+                int visited = Integer.parseInt(information[4]);
+                floorPlan[rowNum][colNum] = new FloorUnit(dirtLevel, obstacle, visited);
                 line = reader.readLine();
             }
             System.out.println("Floor plan recognized!");
