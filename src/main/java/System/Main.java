c8e0109b62cf35a5368373d0c224d03257d4072f
shashanksrikanth1 <ssrikan1@mail.depaul.edu>
2019-10-28 19:32:52 -0500

    Hotfix for path

--- src/main/java/System/Main.java
+++ src/main/java/System/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[]args) throws IOException {
         SensorSystem sensor = new SensorSystem();
-        sensor.readFile("/Users/shashanksrikanth/Desktop/floorplan.txt");
+        sensor.readFile("src/main/java/System/floorplan.txt");
         ControlSystem control = new ControlSystem(sensor);
         while(control.moveDevice()) control.moveDevice();
     }
